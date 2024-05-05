9073d38a190f0a7b650781151462a924179ee186
darklord007 <omraj7240@gmail.com>
2024-05-05 21:37:30 +0530

    ee

--- src/utils/constant.js
+++ src/utils/constant.js
@@ -1,3 +1,9 @@
-export const USER_API_END_POINT="http://localhost:8080/api/v1/user"
-export const TWEET_API_END_POINT="http://localhost:8080/api/v1/tweet"
-export const COMMENT_API_END_POINT="http://localhost:8080/api/v1/comment"
+export const USER_API_END_POINT="https://devmedia-backend.vercel.app/api/v1/user"
+export const TWEET_API_END_POINT="https://devmedia-backend.vercel.app/api/v1/tweet"
+export const COMMENT_API_END_POINT="https://devmedia-backend.vercel.app/api/v1/comment"
+
+
+
+// export const USER_API_END_POINT="http://localhost:8080/api/v1/user"
+// export const TWEET_API_END_POINT="http://localhost:8080/api/v1/tweet"
+// export const COMMENT_API_END_POINT="http://localhost:8080/api/v1/comment"
